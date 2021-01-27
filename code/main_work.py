9b7e03b991255e1f0cde6275bcc295dcbb394b97
wht <17731027375@163.com>
2021-01-27 22:16:11 +0800

    add code file and ensemble method functions

--- code/main_work.py
+++ code/main_work.py
@@ -1,2 +1,13 @@
 import numpy as np
 
+
+def load_data():
+    ori_data = np.load(r'..\data\glass.npy')
+    return ori_data
+
+
+if __name__ == '__main__':
+    # 载入数据
+    data_all = load_data()
+    label = data_all[:, 0]
+    data = data_all[:, 1:]
